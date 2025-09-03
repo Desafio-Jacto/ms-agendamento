2dd5c22b69a09c98b5abbb0f722119dc3f96d5e7
eduardo-granievicz <dudu.grani@hotmail.com>
2025-09-03 11:19:48 -0300

    config: swagger

--- src/main/java/br/com/jacto/api_agendamento/infra/config/OpenApiConfig.java
+++ src/main/java/br/com/jacto/api_agendamento/infra/config/OpenApiConfig.java
@@ -1,26 +1,31 @@
 package br.com.jacto.api_agendamento.infra.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API de Agendamento Jacto",
+                description = "API para gerenciamento de agendamentos e avaliações dos sistemas da Jacto. ",
+                version = "1.0",
+                contact = @Contact(
+                        name = "Equipe de Desenvolvimento",
+                        email = "suporte@jacto.com.br"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                        )
-                );
-    }
 }
